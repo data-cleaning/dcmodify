6c3d4fe49ba698d95de2f4f8255f4b1e6a4303ff
Mark van der Loo <mark.vanderloo@gmail.com>
2019-05-21 12:48:01 +0200

    switched to tinytest

---
+++ pkg/inst/tinytest/test_vectorize.R
@@ -0,0 +1,51 @@
+
+## vectorize
+
+## guarding expressions
+expect_equal(
+    dcmodify:::set_guards( expression(x <- 3)[[1]] )
+  , list( expression(x<-3)[[1]] ) 
+)
+
+L <-  dcmodify:::set_guards(expression(if(x>0) y <- 0)[[1]])
+expect_equal(length(L),1)
+expect_equivalent(L[[1]], expression(y<-0)[[1]])  
+expect_equal(attr(L[[1]],"guard"), expression(x>0)[[1]])
+
+L <- dcmodify:::set_guards(expression(
+ if ( x > 0 ){
+   y <- 1
+ } else {
+   z <- 1
+ }
+)[[1]])  
+expect_equal(length(L),2)
+expect_equivalent(L[[1]],expression(y<-1)[[1]])
+expect_equivalent(L[[2]],expression(z<-1)[[1]])  
+expect_equal(attr(L[[1]],"guard"),expression(x>0)[[1]])
+expect_equal(attr(L[[2]],"guard"),expression(!(x>0))[[1]])
+
+L <- dcmodify:::set_guards(expression(
+  if (x > 0 ){
+    x <- 2
+    y <- 3
+  }
+)[[1]])
+expect_equal(length(L),2)
+expect_equivalent(L[[1]],expression(x <- 2)[[1]])
+expect_equivalent(L[[2]],expression(y <- 3)[[1]])
+expect_equal(attr(L[[1]],"guard"),expression(x>0)[[1]])
+expect_equal(attr(L[[1]],"guard"),expression(x>0)[[1]])
+
+L <- dcmodify:::set_guards(expression(
+  if ( x > 0 ){
+    if (y > 0){
+      z <- 1
+    }
+  }
+)[[1]])
+expect_equivalent(L[[1]],expression(z <- 1)[[1]])
+expect_equal(attr(L[[1]],"guard"),expression((x>0) & (y > 0))[[1]])
+
+
+
